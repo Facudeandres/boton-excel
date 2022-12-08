ef871c4a696f973bc777e516c1c845a2689bf1dd
Facudeandres <115202933+Facudeandres@users.noreply.github.com>
2022-12-07 22:01:20 -0300

    Update excel.py

--- excel.py
+++ excel.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 # Importa la función file_uploader
 from streamlit import file_uploader
@@ -6,7 +7,9 @@ from streamlit import file_uploader
 # Crea el botón y almacena el archivo seleccionado en una variable
 archivo = file_uploader("Selecciona un archivo de Excel:", type="xlsx")
 
-# Si se seleccionó un archivo, muestra su contenido
+# Si se seleccionó un archivo, carga su contenido en un DataFrame de pandas
 if archivo is not None:
     df = pd.read_excel(archivo)
+    
+    # Muestra el contenido del DataFrame en la aplicación
     st.write(df)
